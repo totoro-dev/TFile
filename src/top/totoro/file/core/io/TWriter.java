3119fc95ba38f7dd7d3554865d2ff6353ec9e5d4
totoro-dev <2844150336@qq.com>
2020-02-06 14:22:31 +0800

    将项目迁移为IDEA项目

--- src/top/totoro/file/core/io/TWriter.java
+++ src/top/totoro/file/core/io/TWriter.java
@@ -1,44 +1,42 @@
 package top.totoro.file.core.io;
 
+import top.totoro.file.core.FileProperty;
+
 import java.io.FileWriter;
 import java.io.IOException;
 
-import top.totoro.file.core.FileProperty;
+public class TWriter implements Write {
 
-public class TWriter implements Write{
-	
-	private final FileProperty property;
-	
-	private FileWriter writer;
-	
-	public TWriter(FileProperty property) {
-		this.property = property;
-	}
+    private final FileProperty property;
 
-	@Override
-	public boolean write(String string) {
-		try {
-			writer = new FileWriter(property.getFile().getAbsolutePath());
-//			BufferedWriter bw = new BufferedWriter(writer);
-			writer.write(string);
-//			bw.close();
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return true;
-	}
+    private FileWriter writer;
 
-	@Override
-	public boolean write2JSONObject(String object) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public TWriter(FileProperty property) {
+        this.property = property;
+    }
 
-	@Override
-	public boolean write2JSONArray(String[] items) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean write(String string) {
+        try {
+            writer = new FileWriter(property.getFile().getAbsolutePath());
+            writer.write(string);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean write2JSONObject(String object) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean write2JSONArray(String[] items) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
